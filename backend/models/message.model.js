1c131da6d5954920642e1aa64926ede3f88201de
the-ayushm <ayushm1247@gmail.com>
2026-02-07 13:47:31 +0530

    updated message and conversation schema

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,21 +1,23 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
+    conversationId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Conversation",
+        required: true,
+    },
     senderId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
     },
-    receiverId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    message: {
+    text: {
         type: String,
-        required: true
+        required: true,
     }
 }, { timestamps: true });
+
+messageSchema.index({ conversationId: 1, createdAt: 1 });
 
 const Message = mongoose.model("Message", messageSchema);
 export default Message;
